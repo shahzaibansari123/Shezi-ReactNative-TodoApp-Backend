f4e9fbb08d22caf2099f51881eeb0a8f10f4aa3d
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-12-18 21:59:24 +0500

    uplaodes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(fileUpload({
     limits: {fileSize: 50 * 1024 * 1024},
-    useTempFiles: true,
+    // useTempFiles: true,
     // tempFileDir: '/tmp'
     
 
